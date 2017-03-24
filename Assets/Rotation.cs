df2930431b1dd1fe76d22693eab045642b1bce4d
Osilos <f.marianacci@gmail.com>
2017-03-24 17:58:20 +0100

    first commit

---
+++ Assets/Rotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Board;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Rotate ()
+    {
+        Board.transform.Rotate(Vector3.forward, 90f * (Random.value > 0.5 ? -1 : 1));
+    }
+
+   // private IEnumerator RotateRoutine ()
+   // {
+
+   // }
+}
