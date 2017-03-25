f3af54dcec87566128d80860cfb34a1137b379bb
Théo Sabattié <t.sabattie@student.isartdigital.com>
2017-03-25 15:02:51 +0100

    Fix Color size (prevent blank or rotation and on other screen size)

---
+++ Assets/Scripts/ColorScaler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ColorScaler : MonoBehaviour {
+	private void Start () {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        float size                  = Mathf.Sqrt(Mathf.Pow(Mathf.Max(Screen.width, Screen.height), 2) * 2)/2 + 5;
+        rectTransform.sizeDelta     = Vector3.one * size;
+    }
+}
