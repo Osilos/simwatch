1f943a6ca39f41c628b2a09458e16792f01130e2
Théo Sabattié <t.sabattie@student.isartdigital.com>
2017-03-25 11:55:06 +0100

    Random 90, 180 or 270 for rotation instead of alway 90 or 270

--- Assets/Scripts/GameScreen.cs
+++ Assets/Scripts/GameScreen.cs
@@ -55,7 +55,7 @@ public class GameScreen : CanvasScreen
 
     private void RotationButton_OnClicked()
     {
-        rotator.Rotate(Quaternion.AngleAxis(90 * (Random.value > 0.5 ? -1 : 1), Vector3.forward) * rotator.transform.rotation, rotationSecondsDuration);
+        rotator.Rotate(Quaternion.AngleAxis(90 * Random.Range(1, 4), Vector3.forward) * rotator.transform.rotation, rotationSecondsDuration);
     }
 
     private void AddButtonClickListener(Button button, int index)
